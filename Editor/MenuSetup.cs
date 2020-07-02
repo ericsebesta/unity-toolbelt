1a351b164f81f3a2a4539b31f8b4e1d1accb808d
Eric Sebesta <esebesta@protonmail.com>
2020-07-01 23:05:05 -0500

    add menu item

---
+++ Editor/MenuSetup.cs
@@ -0,0 +1,13 @@
+﻿using com.ericsebesta.unitytools;
+using UnityEditor;
+using UnityEngine;
+
+public class MenuSetup : MonoBehaviour
+{
+    [MenuItem("GameObject/EASTools/ExclusiveChooser")]
+    private static void CreateRedBlueGameObject()
+    {
+        var go = new GameObject ("ExclusiveChooser");
+        go.AddComponent<ExclusiveChooser>();
+    }
+}
