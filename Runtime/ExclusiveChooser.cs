5f88d8b0f759b47c02adc1659d22bfe068b93df9
Eric Sebesta <esebesta@protonmail.com>
2020-07-03 17:32:12 -0500

    fix issue 1 (deactivating the first child throws an error)

--- Runtime/ExclusiveChooser.cs
+++ Runtime/ExclusiveChooser.cs
@@ -15,9 +15,59 @@ namespace com.ericsebesta.toolbelt
         private GameObject m_chosenChild;
         //Whether we are in the process of choosing an active child or not (necessary to prevent re-entry issues from callbacks).
         private bool m_currentlyChoosing;
-        
+        //handle an edge case with deactivating the first child. @see ReActivate 
         private GameObject m_reactivationTarget;
 
+        /// <summary>
+        /// Select the given child as the active chosen one, disable all others. If null, we'll choose the first child.
+        /// The result of any call to this method will be exactly 1 child being activated, unless there are no children.
+        /// </summary>
+        /// <param name="go">The gameobject to choose if found as a child. Can be null which means "choose the first child"</param>
+        public void ChooseChild(GameObject go)
+        {
+            if (m_currentlyChoosing)
+                return;
+            m_currentlyChoosing = true;
+            var foundSelection = false;
+            for (var i = 0; i < gameObject.transform.childCount; i++)
+            {
+                var child = gameObject.transform.GetChild(i);
+                if (go && child == go.transform)
+                {
+                    var childGameObject = child.gameObject; 
+                    childGameObject.SetActive(true);
+                    foundSelection = true;
+                    m_chosenChild = childGameObject;
+                }
+                else
+                {
+                    child.gameObject.SetActive(false);
+                }
+            }
+
+            if (!foundSelection && gameObject.transform.childCount > 0)
+            {
+                var child = gameObject.transform.GetChild(0);
+                var childGameObject = child.gameObject;
+                if (m_chosenChild == childGameObject)
+                {
+                    //in this specific situation, we are re-activating the node we *think* was already active.
+                    //Most likely, this means that the first child was requested to be deactivated (which logic dictates
+                    //should be reactivated here). Doing so inline of the callstack requesting to DISable it will throw
+                    //an error, so delay the reactivation to a future frame. This could probably be done later in the same
+                    //frame rather than waiting until the next frame.
+                    m_reactivationTarget = childGameObject;
+                    Invoke(nameof(ReActivate), .05f);
+                }
+                else
+                {
+                    childGameObject.SetActive(true);
+                }
+                m_chosenChild = childGameObject;
+            }
+            m_currentlyChoosing = false;
+        }
+        
         /// <summary>
         /// When waking up, ensure that only the FIRST active child remains active, disable all others.
         /// Also ensure that all children have the helper component.
@@ -73,49 +123,14 @@ namespace com.ericsebesta.toolbelt
         }
 
         /// <summary>
-        /// Select the given child as the active chosen one, disable all others. If null, we'll choose the first child.
+        /// This method exists to handle an edge case of deactivating the first child, which should re-enable it.
+        /// This will cause an error if we directly call SetActive(true) within the handling of OnDisable. So we delay
+        /// the call and handle it here.
         /// </summary>
-        /// <param name="go">The gameobject to choose if found as a child. Can be null which means "choose the first child"</param>
-        public void ChooseChild(GameObject go)
-        {
-            if (m_currentlyChoosing)
-                return;
-            m_currentlyChoosing = true;
-            var foundSelection = false;
-            for (var i = 0; i < gameObject.transform.childCount; i++)
-            {
-                var child = gameObject.transform.GetChild(i);
-                if (go && child == go.transform)
-                {
-                    var childGameObject = child.gameObject; 
-                    childGameObject.SetActive(true);
-                    foundSelection = true;
-                    m_chosenChild = childGameObject;
-                }
-                else
-                {
-                    child.gameObject.SetActive(false);
-                }
-            }
-
-            if (!foundSelection && gameObject.transform.childCount > 0)
-            {
-                var child = gameObject.transform.GetChild(0);
-                var childGameObject = child.gameObject;
-                //in this specific situation, we might end up re-activating the first child from within a callstack requesting to DISable it.
-                //directly calling into SetActive will throw and error, so delay the reactivation.
-                m_reactivationTarget = childGameObject;
-                Invoke(nameof(ReActivate), .1f);
-                m_chosenChild = childGameObject;
-            }
-            m_currentlyChoosing = false;
-        }
-
         private void ReActivate()
         {
             m_reactivationTarget.SetActive(true);
             m_reactivationTarget = null;
         }
-
     }
 }
